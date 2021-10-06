b7e2009de23d492b0fdfc6ba944702ad60574b02
Jose Prieto <jprieto@mediomelon.mx>
2021-10-06 11:13:18 -0500

    :lipstick: cambios en estilos para todas las vistas

--- app/js/home.js
+++ app/js/home.js
@@ -8,26 +8,6 @@ gallery.controller("inicio", [
         centerMode: true,
         centerPadding: "60px",
         slidesToShow: 3,
-        responsive: [
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: "40px",
-              slidesToShow: 3,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: false,
-              centerMode: true,
-              centerPadding: "40px",
-              slidesToShow: 1,
-            },
-          },
-        ],
       });
     });
   },
